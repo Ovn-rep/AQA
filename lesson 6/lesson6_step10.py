f9d87c611624455a058fabc3d4f7cc05997a1e8b
Alex Karasev <alex333karasev@yandex.ru>
2025-08-06 01:18:18 +0300

    pravki

--- lesson 6/lesson6_step10.py
+++ lesson 6/lesson6_step10.py
@@ -2,9 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-
 browser = webdriver.Chrome()
-browser.get("http://suninjuly.github.io/registration1.html")
+browser.get("http://suninjuly.github.io/registration2.html")
 
 input1 = browser.find_element(by = "css selector", value = 'input[placeholder="Input your first name"]')
 input1.send_keys("Alex")
@@ -12,7 +11,6 @@ input2 = browser.find_element(by = "css selector", value = 'input[placeholder="I
 input2.send_keys("Karasev")
 input3 = browser.find_element(by = "css selector", value = 'input[placeholder="Input your email"]')
 input3.send_keys("alexx333karasev@yandex.ru")
-
 
 button = browser.find_element(by = "tag name", value = "button")
 button.click()
