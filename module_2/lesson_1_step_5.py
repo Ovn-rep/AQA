8c8928474b9a32c833a85b3ada6f862f948e20b3
Alex Karasev <alex333karasev@yandex.ru>
2025-08-06 02:45:04 +0300

    2

---
+++ module_2/lesson_1_step_5.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  result = math.log(abs(12 * math.sin(int(x))))
+  return result
+
+browser = webdriver.Chrome()
+browser.get('https://suninjuly.github.io/math.html')
+
+text_element = browser.find_element(by = "id", value = 'input_value')
+x = text_element.text
+y = calc(x)
+
+input1 = browser.find_element(by = "tag name", value = "input")
+input1.send_keys(str(y))
+checkbox = browser.find_element(by = "id", value="robotCheckbox")
+checkbox.click()
+radio = browser.find_element(by = "id", value="robotsRule")
+radio.click()
+
+button = browser.find_element(by = "class name", value = "btn.btn-default")
+button.click()
+
+time.sleep(10)
