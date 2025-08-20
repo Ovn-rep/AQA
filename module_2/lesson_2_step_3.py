35193d733858f7c44599de5dc7cd2337d3449297
Alex Karasev <alex333karasev@yandex.ru>
2025-08-20 05:06:54 +0300

    2.2.3

---
+++ module_2/lesson_2_step_3.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+def calc(x, y):
+    return(x + y)
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/selects1.html")
+
+num1 = browser.find_element(by = "id", value = "num1")
+x = int(num1.text)
+num2 = browser.find_element(by = "id", value = "num2")
+y = int(num2.text)
+result = calc(x, y)
+select = Select(browser.find_element(by = "id", value = "dropdown"))
+select.select_by_value(str(result))
+browser.find_element(by = "tag name", value = "button").click()
+
+time.sleep(10)
+
+
+
+
+
+
