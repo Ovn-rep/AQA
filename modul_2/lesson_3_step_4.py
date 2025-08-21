073c3a5db87a96decd72222486bfc50995acfbf6
Alex Karasev <alex333karasev@yandex.ru>
2025-08-21 03:42:23 +0300

    modul2_final

---
+++ modul_2/lesson_3_step_4.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return(math.log(abs(12*math.sin(x))))
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/alert_accept.html")
+
+button1 = browser.find_element(by = "class name", value = "btn.btn-primary")
+button1.click()
+confirm = browser.switch_to.alert
+confirm.accept()
+znachenie = browser.find_element(by = "id", value = "input_value")
+x = int(znachenie.text)
+y = calc(x)
+input1 = browser.find_element(by = "id", value = "answer")
+input1.send_keys(str(y))
+button2 = browser.find_element(by = "class name", value = "btn.btn-primary")
+button2.click()
+
+time.sleep(10)
---
+++ modul_2/lesson_3_step_6.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return(math.log(abs(12*math.sin(x))))
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/redirect_accept.html")
+
+button1 = browser.find_element(by = "class name", value = "trollface.btn.btn-primary")
+button1.click()
+window_name = browser.window_handles[1]
+open_new_window = browser.switch_to.window(window_name)
+znachenie = browser.find_element(by = "id", value = "input_value")
+x = int(znachenie.text)
+y = calc(x)
+input1 = browser.find_element(by = "id", value = "answer")
+input1.send_keys(str(y))
+button2 = browser.find_element(by = "class name", value = "btn.btn-primary")
+button2.click()
+
+time.sleep(10)
