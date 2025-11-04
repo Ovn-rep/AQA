ee9e67eca063c985497a25f18e62184c42457650
Alex Karasev <alex333karasev@yandex.ru>
2025-11-04 06:47:46 +0300

    resume_fixturs

---
+++ modul_3/test_fixture8_1.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture()
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+
+class Test():
+
+    @pytest.mark.smoke
+    def test_1(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    @pytest.mark.regression
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
